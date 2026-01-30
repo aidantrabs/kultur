94abac2bed9cdb133c0d6d862b12c040c199e5a7
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-01-30 13:16:23 -0400

    feat: create memory queries

---
+++ backend/queries/memories.sql
@@ -0,0 +1,20 @@
+-- name: ListMemoriesByFestival :many
+SELECT * FROM memories
+WHERE festival_id = $1 AND status = 'approved'
+ORDER BY submitted_at DESC;
+
+-- name: CreateMemory :one
+INSERT INTO memories (
+    festival_id, author_name, author_email, content, year_of_memory
+) VALUES (
+    $1, $2, $3, $4, $5
+) RETURNING *;
+
+-- name: GetMemoryByID :one
+SELECT * FROM memories
+WHERE id = $1;
+
+-- name: UpdateMemoryStatus :exec
+UPDATE memories
+SET status = $2
+WHERE id = $1;
